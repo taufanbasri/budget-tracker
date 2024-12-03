37a059acbd7b6297fdcc5dacbe6c919c827f4b28
Taufan Prasetyo <taufanremote@gmail.com>
2024-12-03 19:55:39 +0700

    Currency Option

---
+++ src/lib/currencies.ts
@@ -0,0 +1,6 @@
+export const Currencies = [
+    { value: 'IDR', label: 'Rp Rupiah', locale: 'id-ID' },
+    { value: 'USD', label: '$ Dollar', locale: 'en-US' },
+]
+
+export type Currency = typeof Currencies[0]
